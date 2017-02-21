cf8f21255071ab828aa87bc7280afc8129baff28
yangsheng327 <yangsheng327@gmail.com>
2017-02-21 12:29:22 -0500

    Add files via upload

---
+++ DSSA.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#Downhill simplex with simulated annealing
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.close('all')
+
+class DSSA:
+    
+    def __init__(self, lossfun, init_para, coei=[1,2,1./2,1./2], k=1):
+        self.fun = lossfun
+        self.df = init_para.shape[1]
+        self.para = np.zeros([self.df+1, self.df])
+        self.para[:,:] = init_para[:,:]
+        self.loss = np.zeros(self.df+1)
+        self.alpha = coei[0]
+        self.gama = coei[1]
+        self.rou = coei[2]
+        self.delta = coei[3]
+        self.k = k #energy constant
+        for n in range(df+1):
+            self.loss[n] = self.fun(self.para[n,:])
+            
+    #Prob of low<high is true, return true/false        
+    def prob(self, low, high): 
+        P_small = 1./(1+np.exp((low-high)/self.k/self.T))#Boltzman distribution 
+        P_rand = np.random.uniform(0,1)
+        if P_rand< P_small:
+            return True
+        else:
+            return False
+        
+    def order(self):#sort para based on loss function
+        sort_id = np.argsort(self.loss)
+        self.loss = self.loss[sort_id]
+        self.para = self.para[sort_id,:]
+        self.centr = np.mean(self.para[:-1,:], axis=0)
+        self.ref =  self.centr + self.alpha*(self.centr-self.para[-1,:])
+        self.ref_loss = self.fun(self.ref)
+    
+    def reflect(self):#reflection
+        self.para[-1,:] = self.ref
+        self.loss[-1] = self.ref_loss
+          
+    def expand(self):#expansion
+        self.exp = self.centr + self.gama*(self.ref-self.centr)
+        self.exp_loss = self.fun(self.exp)
+        if self.prob(self.exp_loss, self.ref_loss):
+            self.para[-1,:] = self.exp
+            self.loss[-1] = self.exp_loss
+        else:
+            self.para[-1,:] = self.ref
+            self.loss[-1] = self.ref_loss
+    
+    def contract(self):
+        self.contr = self.centr + self.rou*(self.para[-1,:]-self.centr)
+        self.contr_loss = self.fun(self.contr)
+        if self.prob(self.contr_loss, self.loss[-1]):
+            self.para[-1,:] = self.contr
+            self.loss[-1] = self.contr_loss
+        else:#shrink
+            for n in range(df):
+                self.para[n+1:,:] = self.para[0,:] + \
+                    self.delta*(self.para[n+1:,:]-self.para[0,:])
+                self.loss[n+1] = self.fun(self.para[n+1,:])
+               
+    def fit(self, iter_num):
+        for i in range(iter_num):
+            self.T = 1.-i/iter_num
+#            print(self.T)
+            self.order()
+            if self.prob(self.ref_loss, self.loss[-2]):
+                if self.prob(self.loss[0], self.ref_loss):
+                    self.reflect()
+                else:
+                    self.expand()
+            else:
+                self.contract()
+    
+def loss(x):
+    r = np.sqrt(sum(x**2))# + np.random.uniform(0,0.5)
+    return -(np.sin(r)/r)
+#def loss(x):
+#    return sum(x**2)
+
+    
+    
+    
+df = 2#dimension of freedom
+x0 = np.random.uniform(-20,20, [df + 1, df])
+ds = DSSA(loss, x0)
+ds.fit(10000)
+print(ds.para)
+print(ds.loss)
+#print(ds.centr)
